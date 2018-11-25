8fd9df43013247e454ecd37a0a2439fdfaa890ea
Hadi Awad <haawad@cisco.com>
2018-11-25 10:07:16 +0200

    Adding sinX math Function

--- math/src/test/java/edu/birzeit/math/SimpleMathTest.java
+++ math/src/test/java/edu/birzeit/math/SimpleMathTest.java
@@ -1,37 +1,43 @@
 package edu.birzeit.math;
 
-import org.junit.After;
-import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import junit.framework.TestCase;
 
 public class SimpleMathTest extends TestCase {
-	
+
 	static SimpleMath simpleMath;
-	
+
 	@BeforeClass
 	public static void initCalculator() {
-		simpleMath= new SimpleMath();
+		simpleMath = new SimpleMath();
 	}
-		
-	    @Test
-		    public void testSum() {
+
+	@Test
+	public void testSum() {
+		int result = simpleMath.add(10, 10);
+		assertEquals(20, result);
+
+	}
+
+	@Test
+	public void testSumIntegerDouble() {
+		@SuppressWarnings("static-access")
+		Double result = simpleMath.add(10.5, 10.5);
+		assertEquals(21, result);
+
+	}
 	
-	        int result = simpleMath.add(10, 10);	
-	        assertEquals(20, result);
-	
-	    }
-
-	    @Test
-	    public void testSumIntegerDouble() {
-
-        @SuppressWarnings("static-access")
-		Double result =  simpleMath.add(10.5, 10.5);	
-        assertEquals(21, result);
-
+	@Test
+    public void testSinX_hadiAwad() {
+        assertEquals(0, simpleMath.sinX_hadiAwad(0));
+        assertEquals(1, simpleMath.sinX_hadiAwad(90));
+        assertEquals(0, simpleMath.sinX_hadiAwad(180));
+        assertEquals(-1, simpleMath.sinX_hadiAwad(270));
+        assertEquals(0, simpleMath.sinX_hadiAwad(360));
     }
-	
 
 }
